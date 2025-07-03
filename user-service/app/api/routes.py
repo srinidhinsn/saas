6693523058c9d781e0eda2e864d23ddaf18c7ff7
Srinidhi <srinidhinsn@gmail.com>
2025-07-04 04:22:31 +0530

    setting response

--- user-service/app/api/routes.py
+++ user-service/app/api/routes.py
@@ -30,7 +30,7 @@ async def login_user(client_id: str, userReq: LoginRequest, db: Session = Depend
     if not userModel or not verify_password(userReq.password, userModel.hashed_password):
         raise HTTPException(status_code=400, detail="Invalid credentials")
     token = create_access_token({"user_id": str(userModel.id), "roles": userModel.roles, "client_id": userModel.client_id, "grants" : userModel.grants})
-    response = ResponseModel(screen_id="defaultUser", data={"access_token": token, "token_type": "bearer"})
+    response = ResponseModel(screen_id="default_user", data={"access_token": token, "token_type": "bearer"})
     #response.set_response(screen_id="defaultUser", data={"access_token": token, "token_type": "bearer"})
     return response
 
@@ -40,16 +40,20 @@ async def login_user(client_id: str, userReq: LoginRequest, db: Session = Depend
 async def test_msg(client_id: str, context: SaasContext = Depends(verify_token), db: Session = Depends(get_db)):
     print("test context - ", context)
     print("test screen_id - ", context.screen_id)
-    return {"message": "Test Authentication Service Running in user routes"}
+    response = ResponseModel(screen_id="default_test", data={"message": "Test Authentication Service Running in user routes"})
+    return response
      
 
 
 @router.get("/test2")
 async def test_msg(client_id: str, context: SaasContext = Depends(verify_token), db: Session = Depends(get_db)):
     print("test context2 - ", context)
-    return {"message": "Test2 Authentication Service Running in user routes"}
+    response = ResponseModel(screen_id="default_test", data={"message": "Test2 Authentication Service Running in user routes"})
+    return response
+    
 
 @router.get("/test3", include_in_schema=False)
 async def test_msg(client_id: str, context: SaasContext = Depends(verify_token), db: Session = Depends(get_db)):
     print("test context3 - ", context)
-    return {"message": "Test3 Authentication Service Running in user routes"}
+    response = ResponseModel(screen_id="default_test", data={"message": "Test3 Authentication Service Running in user routes"})
+    return response
