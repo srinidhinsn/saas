d899c61bd1c8bda1a6946b72f8f4639956f03f01
Srinidhi <srinidhinsn@gmail.com>
2025-10-04 00:26:39 +0530

    test

--- common-lib/database/postgres.py
+++ common-lib/database/postgres.py
@@ -1,19 +1,18 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, session
 import psycopg2
+from contextlib import contextmanager
 
+# Create a connection once (not callable)
 conn = psycopg2.connect(
-    host="saas-473815:asia-south2:saas", # Or private IP if using private connectivity
+    host="saas-473815:asia-south2:saas",  # Use IP if needed
     database="saas",
     user="postgres",
     password="Saasqa@123",
-    port="5432" # Default PostgreSQL port
+    port="5432"
 )
 
+@contextmanager
 def get_db():
-    db = conn()
     try:
-        yield db  # Yields session instance for request
+        yield conn
     finally:
-        db.close()  # Ensures session is closed after use
+        conn.close()
