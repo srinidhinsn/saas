2dc4c799e442f993f7c095a1a834407cc668a1ce
Srinidhi <srinidhinsn@gmail.com>
2025-10-04 00:23:58 +0530

    alignment fix

--- common-lib/database/postgres.py
+++ common-lib/database/postgres.py
@@ -3,13 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, session
 import psycopg2
 
-    conn = psycopg2.connect(
+
+conn = psycopg2.connect(
         host="saas-473815:asia-south2:saas", # Or private IP if using private connectivity
         database="saas",
         user="postgres",
         password="Saasqa@123",
         port="5432" # Default PostgreSQL port
-    )
+)
 
 def get_db():
     db = conn()
