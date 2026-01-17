9f14b1f1fa0e70340720e939cb18221180b5777c
Srinidhi <srinidhinsn@gmail.com>
2026-01-17 23:03:40 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/foodee/',
+  base: '/foodee/assets/',
   plugins: [react()],
   build: {
     rollupOptions: {
