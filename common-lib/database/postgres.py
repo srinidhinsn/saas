965baa269dad3f4906e5ea5e29d4ce92099a83f0
Srinidhi N <srinidhinsn@gmail.com>
2026-01-30 13:34:51 +0530

    DB Connector

--- common-lib/database/postgres.py
+++ common-lib/database/postgres.py
@@ -1,55 +1,55 @@
-# from sqlalchemy import create_engine, Column, Integer, String
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker, session
-# from dotenv import load_dotenv
-# import os
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, session
+from dotenv import load_dotenv
+import os
 
-# load_dotenv()
-# DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base = declarative_base()
-
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db  # Yields session instance for request
-#     finally:
-#         db.close()  # Ensures session is closed after use
+Base = declarative_base()
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  Yields session instance for request
+    finally:
+        db.close()  Ensures session is closed after use
 
 
 
 
 
+
+'''
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-# Load .env
+Load .env
 load_dotenv()
 
-# Read credentials
+Read credentials
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
 
-# Build DATABASE_URL
+Build DATABASE_URL
 DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 
-# SQLAlchemy setup
+SQLAlchemy setup
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
@@ -61,10 +61,11 @@ SessionLocal = sessionmaker(
 Base = declarative_base()
 
 
-# Dependency
+Dependency
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()  # Ensures session is closed after use
+        db.close()  Ensures session is closed after use
+'''
