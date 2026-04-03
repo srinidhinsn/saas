35c9b437a775171ac7d8f56ef8a125fe5b0cf2a2
Prajwal <prajwalbb3@gmail.com>
2026-04-03 16:12:33 +0530

    d

--- common-lib/database/postgres.py
+++ common-lib/database/postgres.py
@@ -1,17 +1,71 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, session
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/saas"
+load_dotenv(dotenv_path="/app/.env")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 
 def get_db():
     db = SessionLocal()
     try:
-        yield db  # Yields session instance for request
+        yield db
     finally:
-        db.close()  # Ensures session is closed after use
+        db.close()
+
+
+
+
+
+
+'''
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+Load .env
+load_dotenv()
+
+Read credentials
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
+
+Build DATABASE_URL
+DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+SQLAlchemy setup
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()  Ensures session is closed after use
+'''
